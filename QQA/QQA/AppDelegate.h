4a8dc7da466631c2013fd7792239ab42c4134164
huiming <1606166468@qq.com>
2017-10-30 04:11:23 +0800

    change  youth-firstday

---
+++ QQA/QQA/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  QQA
+//
+//  Created by wang huiming on 2017/10/30.
+//  Copyright © 2017年 youth_huiming. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
