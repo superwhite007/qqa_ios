5be70e363732b58894e971a089dd18d3f9d6a71f
huiming <youthprajna@126.com>
2017-11-15 09:25:53 +0800

    change me pages

--- QQA/QQA/sections/New Group5/MeInformationViewController.m
+++ QQA/QQA/sections/New Group5/MeInformationViewController.m
@@ -21,11 +21,24 @@
 @property (nonatomic, strong) NSString * qq;
 @property (nonatomic, strong) NSString * wechat;
 
+@property (nonatomic, strong) UIView * view;
+
 @end
 
 @implementation MeInformationViewController
 
-
+-(NSString *)imgUrl{
+    if (!_imgUrl) {
+        self.imgUrl = [NSString new];
+    }
+    return _imgUrl;
+}
+-(NSString *)nameStr{
+    if (_nameStr) {
+        self.nameStr = [NSString new];
+    }
+    return _nameStr;
+}
 
 
 
@@ -34,54 +47,57 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view.
     
-    self.view.backgroundColor = [UIColor greenColor];
+//    self.view.backgroundColor = [UIColor greenColor];
     
     
-    UIView *view = [[UIView alloc ] initWithFrame:CGRectMake(0, 64, iphoneWidth, iphoneWidth * 2 / 3)];
-    view.backgroundColor = [UIColor colorWithRed:arc4random() % 256 / 255.0 green:arc4random() % 256 / 255.0 blue:arc4random() % 256 / 255.0 alpha:1];
-    [self.view addSubview: view];
+//    UIView *view = [[UIView alloc ] initWithFrame:CGRectMake(0, 64, iphoneWidth, iphoneWidth * 2 / 3)];
+////    view.backgroundColor = [UIColor colorWithRed:arc4random() % 256 / 255.0 green:arc4random() % 256 / 255.0 blue:arc4random() % 256 / 255.0 alpha:1];
+//    [self.view addSubview: view];
     
     
-    UIButton * punchCLockImageTileButton = [UIButton buttonWithType:UIButtonTypeSystem];
-    punchCLockImageTileButton.frame = CGRectMake(0, 0, [[UIScreen mainScreen] bounds].size.width, [[UIScreen mainScreen] bounds].size.width * 2 / 3);
-    punchCLockImageTileButton.backgroundColor = [UIColor redColor];
-    [punchCLockImageTileButton setBackgroundImage:[UIImage imageNamed:@"Me"] forState:UIControlStateNormal];
-    //    [view addSubview:punchCLockImageTileButton];
     
-    
-    NSArray * labelNameArray = @[ @"imageString", @"姓名", @"名称", @"部门", @"职位", @"NO.", @"昵称", @"轻轻ID:"];
-    UIImageView * imgVIew = [[UIImageView alloc] initWithFrame:CGRectMake(20, iphoneWidth  / 9 , iphoneWidth * 4 / 9 , iphoneWidth * 4 / 9)];
-    imgVIew.backgroundColor = [UIColor redColor];
-    imgVIew.layer.cornerRadius = imgVIew.frame.size.width/2;
-    
-    imgVIew.clipsToBounds = YES;
-    //    UIImage *image = [UIImage imageNamed:labelNameArray[0]]; hongjinbao
-    UIImage *image = [UIImage imageNamed:@"hongjinbao"];
-    [imgVIew setImage:image];
-    [view addSubview:imgVIew];
-    
-    for (int i = 1; i < 7; i++) {
-        UILabel * label = [[UILabel alloc] init];
-        if ( i > 0 && i < 7) {
-            label.frame = CGRectMake(iphoneWidth * 5 / 9, iphoneWidth * 4 / 9  / 7 + ( iphoneWidth * 2 / 3 / 7  * (i - 1)), iphoneWidth  / 3, iphoneWidth * 4 / 9 / 7);
-            
-        }
-        label.backgroundColor = [UIColor blueColor];
-        [label setText:labelNameArray[i]];
-        [view addSubview:label];
-    }
+//    NSArray * labelNameArray = @[ @"imageString", @"姓名", @"名称", @"部门", @"职位", @"NO.", @"昵称", @"轻轻ID:"];
+//    UIImageView * imgVIew = [[UIImageView alloc] initWithFrame:CGRectMake(20, iphoneWidth  / 9 , iphoneWidth * 4 / 9 , iphoneWidth * 4 / 9)];
+//    imgVIew.backgroundColor = [UIColor redColor];
+//    imgVIew.layer.cornerRadius = imgVIew.frame.size.width/2;
+//
+//    imgVIew.clipsToBounds = YES;
+//    //    UIImage *image = [UIImage imageNamed:labelNameArray[0]]; hongjinbao
+//
+//    NSData *data = [NSData dataWithContentsOfURL:[NSURL URLWithString:@"http://172.19.12.6/v1/api/user/getImg/ff66070a3e8abc40d24f905467123d92-afcbc2ed6ed1896b1e9f8df03abad60c.jpg"]];
+//    UIImage *image = [UIImage imageWithData:data];
+////    NSLog(@"w = %f,h = %f",image.size.width,image.size.height);
+////
+////    UIImage *image = [UIImage imageNamed:@"hongjinbao"];
+//    [imgVIew setImage:image];
+//    [view addSubview:imgVIew];
+//
+//    for (int i = 1; i < 7; i++) {
+//        UILabel * label = [[UILabel alloc] init];
+//        if ( i > 0 && i < 7) {
+//            label.frame = CGRectMake(iphoneWidth * 5 / 9, iphoneWidth * 4 / 9  / 7 + ( iphoneWidth * 2 / 3 / 7  * (i - 1)), iphoneWidth  / 3, iphoneWidth * 4 / 9 / 7);
+//
+//        }
+//        label.backgroundColor = [UIColor blueColor];
+//        [label setText:labelNameArray[i]];
+//        [view addSubview:label];
+//    }
     
     
     
     UIButton * button1 = [UIButton buttonWithType:UIButtonTypeSystem];
-    button1.frame = CGRectMake(0, CGRectGetMaxY(view.frame) + 10, iphoneWidth, 60);
+    button1.frame = CGRectMake(0, iphoneWidth * 2 / 3 + 64 + 10, iphoneWidth, 60);
     button1.backgroundColor = [UIColor darkGrayColor];
     [button1 setTitle:@"发起通知" forState:(UIControlStateNormal)];
+    button1.contentHorizontalAlignment = UIControlContentHorizontalAlignmentLeft;
+    button1.titleEdgeInsets = UIEdgeInsetsMake(0, 20, 0, 0);  
     [self.view addSubview:button1];
     
     UIButton * button2 = [UIButton buttonWithType:UIButtonTypeSystem];
     button2.frame = CGRectMake(0, CGRectGetMaxY(button1.frame) + 5, iphoneWidth, 60);
     button2.backgroundColor = [UIColor darkGrayColor];
+    button2.contentHorizontalAlignment = UIControlContentHorizontalAlignmentLeft;
+    button2.titleEdgeInsets = UIEdgeInsetsMake(0, 20, 0, 0);
     [button2 setTitle:@"修改登录密码" forState:(UIControlStateNormal)];
     [self.view addSubview:button2];
     
@@ -89,18 +105,19 @@
     button3.frame = CGRectMake(0, CGRectGetMaxY(button2.frame) + 5, iphoneWidth, 60);
     button3.backgroundColor = [UIColor darkGrayColor];
     [button3 setTitle:@"关于青春" forState:(UIControlStateNormal)];
+    button3.contentHorizontalAlignment = UIControlContentHorizontalAlignmentLeft;
+    button3.titleEdgeInsets = UIEdgeInsetsMake(0, 20, 0, 0);
     [button3 addTarget:self action:@selector(gotoAboutQingqing) forControlEvents:(UIControlEventTouchUpInside)];
     [self.view addSubview:button3];
     
     
+    [self punchRecoret];
     
-    
-    
-    
-    
-//    [self punchRecoret];
+
     
 }
+
+
 
 -(void)gotoAboutQingqing{
     AboutYouthViewController * aboutYouthVC = [AboutYouthViewController new];
@@ -111,7 +128,9 @@
 
 -(void)punchRecoret{
     
-    NSURL * url = [NSURL URLWithString:[NSString stringWithFormat:@"http://172.19.12.6/v1/api/attendance/index"]];
+    __block NSDictionary * dictBack = [NSDictionary dictionary];
+    
+    NSURL * url = [NSURL URLWithString:[NSString stringWithFormat:@"http://172.19.12.6/v1/api/user/show"]];
     NSMutableURLRequest * request = [NSMutableURLRequest requestWithURL:url];
     [request setValue:@"application/json" forHTTPHeaderField:@"Content-Type"];
     request.timeoutInterval = 10.0;
@@ -126,7 +145,7 @@
     
     [request setValue:resultDicAccess[@"access_token"] forHTTPHeaderField:@"Authorization"];
     
-    [mdict setObject:@"1" forKey:@"pageNum"];
+//    [mdict setObject:@"1" forKey:@"pageNum"];
     [mdict setObject:@"IOS_APP" forKey:@"client_type"];
     
     NSError * error = nil;
@@ -144,48 +163,69 @@
                                             
                                             if (data != nil) {
                                                 
-                                                NSArray *array1 = [NSJSONSerialization JSONObjectWithData:data options:kNilOptions error:nil];
-                                                NSMutableArray * array = [[NSMutableArray alloc] initWithArray:array1];
-                                                NSLog(@"array2%@", array);
-                                                NSDictionary * firDict = array[0];
-                                                NSString * str  = [NSString stringWithFormat:@"%@", [firDict objectForKey:@"message"]];
-                                                if ([str isEqualToString:@"3004" ]) {
+                                                NSDictionary *dict = [NSJSONSerialization JSONObjectWithData:data options:kNilOptions error:nil];
+                                               NSLog(@"dict: %@", dict);
+                                                
+//                                                [self  gitSomeThingsdictionary:dict];
+                                                dispatch_async(dispatch_get_main_queue(), ^{
+                                                     [self  gitSomeThingsdictionary:dict];
                                                     
-                                                    [array removeObjectAtIndex:0];
-                                                    
-                                                    //                                                    NSLog(@"PunchRecord1:%@", array);
-                                                    
-//                                                    self.datasource = array;
-//
-//                                                    dispatch_async(dispatch_get_main_queue(), ^{
-//                                                        self.datasource = array;
-//                                                        [self.aTableView reloadData];
-//
-//                                                    });
-                                                    
-                                                }
+                                                });
+                                                
+
                                                 
                                             } else{
-                                                NSLog(@"获取数据失败，问李鹏");
+                                                NSLog(@"获取数据失败，问");
                                             }
                                             
                                         }];
     
     [task resume];
-    
-    
+   
+
 }
 
 
-
-
-
-
-
-
-
-
-
+-(void)gitSomeThingsdictionary:(NSDictionary *)dict{
+    
+    NSLog(@"dict: %@", dict);
+    
+    UIView *view = [[UIView alloc ] initWithFrame:CGRectMake(0, 64, iphoneWidth, iphoneWidth * 2 / 3)];
+    view.backgroundColor = [UIColor colorWithRed:arc4random() % 256 / 255.0 green:arc4random() % 256 / 255.0 blue:arc4random() % 256 / 255.0 alpha:1];
+    
+    [self.view addSubview: view];
+    
+    NSArray * labelNameArray = @[@"imageString", @"姓名:", @"部门：", @"职位：", @"性别：", @"NO.", @"电话：", @"email:", @"QQ:", @"WeChat:"];
+    NSArray * urlRebackArray = @[[dict objectForKey:@"avatar"] , [dict objectForKey:@"username"], [dict objectForKey:@"departments"], [dict objectForKey:@"jobs"], [dict objectForKey:@"number"], [dict objectForKey:@"number"], [dict objectForKey:@"telephone"], [dict objectForKey:@"email"], [dict objectForKey:@"number"], [dict objectForKey:@"number"]];
+    
+    
+    
+    
+    UIImageView * imgVIew = [[UIImageView alloc] initWithFrame:CGRectMake(15, iphoneWidth  / 9 , iphoneWidth * 4 / 9 , iphoneWidth * 4 / 9)];
+    imgVIew.backgroundColor = [UIColor redColor];
+    imgVIew.layer.cornerRadius = imgVIew.frame.size.width/2;
+    
+    imgVIew.clipsToBounds = YES;
+    //    UIImage *image = [UIImage imageNamed:labelNameArray[0]]; hongjinbao
+    
+    NSData *data = [NSData dataWithContentsOfURL:[NSURL URLWithString:[dict objectForKey:@"avatar"]]];
+    UIImage *image = [UIImage imageWithData:data];
+    [imgVIew setImage:image];
+    [view addSubview:imgVIew];
+   
+    
+    for (int i = 1; i < 10; i++) {
+        UILabel * label = [[UILabel alloc] init];
+        if ( i > 0 && i < 10) {
+            label.frame = CGRectMake(iphoneWidth * 4 / 9 + 25, iphoneWidth * 2 / 3 / 10 / 2 + ( iphoneWidth * 2 / 3 / 10  * (i - 1)), iphoneWidth  / 2 -20,  iphoneWidth * 2 / 3 / 10);
+            
+        }
+//        label.backgroundColor = [UIColor blueColor];
+        label.adjustsFontSizeToFitWidth = YES;
+        [label setText:[NSString stringWithFormat:@"%@%@", labelNameArray[i], urlRebackArray[i]]];
+        [view addSubview:label];
+    }
+}
 
 
 
