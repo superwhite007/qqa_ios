b45cf5603b5a4e5a9725c146eee6ab1a98920325
huiming <youthprajna@126.com>
2017-11-21 13:42:09 +0800

    delete code in messageVC and add some code in sendVC

--- QQA/QQA/sections/New Group5/MessageViewController.m
+++ QQA/QQA/sections/New Group5/MessageViewController.m
@@ -70,76 +70,11 @@
     rangeOfTransmissionLabel.layer.cornerRadius = 5;
 //    [self.view addSubview:rangeOfTransmissionLabel];
     
-    
-    
-    [self gitMessageAboutGiveNotices];
-    
-    
-//    [self sendRangeoftransmission];
+ 
+ 
 //    [self scrlollVIew];
     
 }
-
-
--(void)gitMessageAboutGiveNotices{
-    
-    NSURL * url = [NSURL URLWithString:[NSString stringWithFormat:@"http://172.19.12.6/v1/api/message/scope"]];
-    NSMutableURLRequest * request = [NSMutableURLRequest requestWithURL:url];
-    [request setValue:@"application/json" forHTTPHeaderField:@"Content-Type"];
-    request.timeoutInterval = 10.0;
-    request.HTTPMethod = @"POST";
-    
-    NSString *sTextPath = [NSHomeDirectory() stringByAppendingPathComponent:@"Documents/bada.txt"];
-    NSDictionary *resultDic = [NSDictionary dictionaryWithContentsOfFile:sTextPath];
-    NSString *sTextPathAccess = [NSHomeDirectory() stringByAppendingPathComponent:@"Documents/badaAccessToktn.txt"];
-    NSDictionary *resultDicAccess = [NSDictionary dictionaryWithContentsOfFile:sTextPathAccess];
-    
-    
-    NSLog(@"resultDic, resultDicAccess:%@, %@", resultDic, resultDicAccess);
-    
-    NSMutableDictionary * mdict = [NSMutableDictionary dictionaryWithDictionary:resultDic];
-    [request setValue:resultDicAccess[@"access_token"] forHTTPHeaderField:@"Authorization"];
-    [mdict setObject:@"IOS_APP" forKey:@"client_type"];
-    
-    NSError * error = nil;
-    NSData * jsonData = [NSJSONSerialization dataWithJSONObject:mdict options:NSJSONWritingPrettyPrinted error:&error];
-    request.HTTPBody = jsonData;
-    
-    NSURLSession *session = [NSURLSession sharedSession];
-    // 由于要先对request先行处理,我们通过request初始化task
-    NSURLSessionTask *task = [session dataTaskWithRequest:request
-                                        completionHandler:^(NSData *data, NSURLResponse *response, NSError *error) {
-                                            
-                                            //                                            NSLog(@"response, error :%@, %@", response, error);
-                                            //                                            NSLog(@"data:%@", data);
-                                            
-                                            if (data != nil) {
-                                                
-                                                NSDictionary *dict = [NSJSONSerialization JSONObjectWithData:data options:kNilOptions error:nil];
-                                                NSLog(@"MessageViewControllerdict: %@", dict);
-                                                NSMutableArray * array = dict[@"departments"];
-                                                
-                                                
-                                                
-                                                
-//                                                dispatch_async(dispatch_get_main_queue(), ^{
-////                                                    [self  gitSomeThingsdictionary:dict];
-//
-//                                                });
-                                                
-                                            } else{
-                                                NSLog(@"获取数据失败，问");
-                                            }
-                                        }];
-    [task resume];
-    
-}
-
-
-
-
-
-
 
 
 
@@ -196,22 +131,8 @@
 }
 
 
-
-
-
-
--(void)sendRangeoftransmission{
- 
-    
-}
-
-
 - (void)btnClick:(UIButton *)sender{
     
-    //    NSLog(@"sender::%ld, %@", (long)sender.tag, sender.titleLabel.text);
-    ////    self.mutableArray = [NSMutableArray new];
-    //    [self.mutableArray addObject:sender.titleLabel.text];
-    //    NSLog(@" mutableArray%@", _mutableArray);
     
     NSString *str = sender.titleLabel.text;
     BOOL isbool = [_mutableArray containsObject: str];
@@ -233,17 +154,12 @@
 }
 
 
-
-
-
 -(void)sends{
     
      [_messageTextView resignFirstResponder];
     [self sendNoticeToServer];
   
-    
 }
-
 
 
 -(BOOL)textView:(UITextView *)textView shouldChangeTextInRange:(NSRange)range replacementText:(NSString *)text
@@ -258,7 +174,6 @@
         [self alert:@"最多输入200字符"];
         return NO;
     }
-    
     
     return YES;
     
@@ -320,14 +235,6 @@
     
     
 }
-
-
-
-
-
-
-
-
 
 
 -(void)chageColor{
--- QQA/QQA/sections/New Group5/SendTheScopeViewController.m
+++ QQA/QQA/sections/New Group5/SendTheScopeViewController.m
@@ -25,6 +25,71 @@
     
 }
 
+-(void)gitMessageAboutGiveNotices{
+    
+    NSURL * url = [NSURL URLWithString:[NSString stringWithFormat:@"http://172.19.12.6/v1/api/message/scope"]];
+    NSMutableURLRequest * request = [NSMutableURLRequest requestWithURL:url];
+    [request setValue:@"application/json" forHTTPHeaderField:@"Content-Type"];
+    request.timeoutInterval = 10.0;
+    request.HTTPMethod = @"POST";
+    
+    NSString *sTextPath = [NSHomeDirectory() stringByAppendingPathComponent:@"Documents/bada.txt"];
+    NSDictionary *resultDic = [NSDictionary dictionaryWithContentsOfFile:sTextPath];
+    NSString *sTextPathAccess = [NSHomeDirectory() stringByAppendingPathComponent:@"Documents/badaAccessToktn.txt"];
+    NSDictionary *resultDicAccess = [NSDictionary dictionaryWithContentsOfFile:sTextPathAccess];
+    
+    
+    NSLog(@"resultDic, resultDicAccess:%@, %@", resultDic, resultDicAccess);
+    
+    NSMutableDictionary * mdict = [NSMutableDictionary dictionaryWithDictionary:resultDic];
+    [request setValue:resultDicAccess[@"access_token"] forHTTPHeaderField:@"Authorization"];
+    [mdict setObject:@"IOS_APP" forKey:@"client_type"];
+    
+    NSError * error = nil;
+    NSData * jsonData = [NSJSONSerialization dataWithJSONObject:mdict options:NSJSONWritingPrettyPrinted error:&error];
+    request.HTTPBody = jsonData;
+    
+    NSURLSession *session = [NSURLSession sharedSession];
+    // 由于要先对request先行处理,我们通过request初始化task
+    NSURLSessionTask *task = [session dataTaskWithRequest:request
+                                        completionHandler:^(NSData *data, NSURLResponse *response, NSError *error) {
+                                            
+                                            //                                            NSLog(@"response, error :%@, %@", response, error);
+                                            //                                            NSLog(@"data:%@", data);
+                                            
+                                            if (data != nil) {
+                                                
+                                                NSDictionary *dict = [NSJSONSerialization JSONObjectWithData:data options:kNilOptions error:nil];
+                                                NSLog(@"MessageViewControllerdict: %@", dict);
+                                                NSMutableArray * array = dict[@"departments"];
+                                                
+                                                
+                                                
+                                                
+                                                //                                                dispatch_async(dispatch_get_main_queue(), ^{
+                                                ////                                                    [self  gitSomeThingsdictionary:dict];
+                                                //
+                                                //                                                });
+                                                
+                                            } else{
+                                                NSLog(@"获取数据失败，问");
+                                            }
+                                        }];
+    [task resume];
+    
+}
+
+
+
+
+
+
+
+
+
+
+
+
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
