a81eee68fe6f2f131c24644e542ba01a22892a83
huiming <youthprajna@126.com>
2018-05-02 13:57:36 +0800

    change display name

--- QQA/QQA/sections/New Group5/MeInformationViewController.m
+++ QQA/QQA/sections/New Group5/MeInformationViewController.m
@@ -34,7 +34,7 @@
 
 
 //    self.navigationController.navigationBar.barTintColor = [UIColor colorWithRed:238  / 255.0 green:0  / 255.0 blue:0 / 255.0 alpha:0.5];
-    NSArray * titleArray = [NSArray arrayWithObjects:@"发起通知", @"版本更新", @"关于青春",  nil];
+    NSArray * titleArray = [NSArray arrayWithObjects:@"发起通知", @"版本更新", @"关于青青",  nil];
     for (int i = 0; i < [titleArray count]; i++) {
         UIButton * button = [UIButton buttonWithType:UIButtonTypeSystem];
         button.frame = CGRectMake(35, iphoneWidth * 2 / 3 + 10 + i * 60, iphoneWidth - 35, 60);
