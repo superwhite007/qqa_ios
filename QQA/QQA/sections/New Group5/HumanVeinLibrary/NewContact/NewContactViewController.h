56963c2119c730ca8455cf0aa0af5de92b5156ac
huiming <youthprajna@126.com>
2018-05-29 11:17:48 +0800

    Perfect newContact

--- QQA/QQA/sections/New Group5/HumanVeinLibrary/NewContact/NewContactViewController.h
+++ QQA/QQA/sections/New Group5/HumanVeinLibrary/NewContact/NewContactViewController.h
@@ -8,6 +8,13 @@
 
 #import <UIKit/UIKit.h>
 
-@interface NewContactViewController : UIViewController
+@interface NewContactViewController : UIViewController<UITextViewDelegate>
+
+@property (nonatomic, strong) UITextView * nameTextView;
+@property (nonatomic, strong) UITextView * describeTextView;
+@property (nonatomic, strong) UITextView * telephoneTextView;
+@property (nonatomic, strong) UITextView * mailTextView;
+@property (nonatomic, strong) UITextView * QQTextView;
+@property (nonatomic, strong) UITextView * weixinTextView;
 
 @end
--- QQA/QQA/sections/New Group5/HumanVeinLibrary/NewContact/NewContactViewController.m
+++ QQA/QQA/sections/New Group5/HumanVeinLibrary/NewContact/NewContactViewController.m
@@ -10,6 +10,9 @@
 
 @interface NewContactViewController ()
 
+@property (nonatomic, strong) NSArray * nameArray;
+@property (nonatomic, strong) NSMutableArray * nameTextViewMArray;
+
 @end
 
 @implementation NewContactViewController
@@ -23,13 +26,63 @@
     UIImageView *imageView = [[UIImageView alloc] initWithFrame:CGRectMake((iphoneWidth - 100)/2, 10, 100, 100)];
     [imageView setImage:[UIImage imageNamed:@"new_contact"]];
     [self.view addSubview:imageView];
+    _nameArray = @[@"姓名", @"描述", @"电话", @"邮件", @"QQ", @"微信"];
+    _nameTextViewMArray = [NSMutableArray arrayWithObjects:_nameTextView, _describeTextView, _telephoneTextView, _mailTextView, _QQTextView, _weixinTextView, nil];
+    [self addNamesAndTextViews];
+    
+    
     
     // Do any additional setup after loading the view.
 }
--(void)ensure{
-    self.view.backgroundColor = [UIColor colorWithRed:arc4random() % 256 / 255.0 green:arc4random() % 256 / 255.0 blue:arc4random() % 256 / 255.0 alpha:1];
+
+-(void)addNamesAndTextViews{
+    if (_nameArray.count > 0) {
+        for (int i = 0; i < _nameArray.count; i++) {
+            UILabel * namesLabel = [[UILabel alloc] initWithFrame:CGRectMake(20, 120 + i * 35 ,iphoneWidth / 3 - 40, 30)];
+            namesLabel.backgroundColor = [UIColor redColor];
+            namesLabel.textAlignment = NSTextAlignmentCenter;
+            namesLabel.text = _nameArray[i];
+            namesLabel.textColor = [UIColor whiteColor];
+            [self.view addSubview:namesLabel];
+            
+            _nameTextViewMArray[i] = [[UITextView alloc] initWithFrame:CGRectMake(iphoneWidth / 3 - 20, 120 + i * 35 ,iphoneWidth * 2 / 3 - 40, 30)];
+//            [_nameTextViewMArray[i] setFont:[UIFont systemFontOfSize:24]];
+            [_nameTextViewMArray[i] setDelegate:self];
+            [self.view addSubview:_nameTextViewMArray[i]];
+        }
+        
+    }
+}
+-(BOOL)textView:(UITextView *)textView shouldChangeTextInRange:(NSRange)range replacementText:(NSString *)text
+{
+    if ([text isEqualToString:@"\n"]) {
+      
+    }else if (range.location >= 200){
+        
+    }
+    return YES;
 }
 
+- (void)textViewDidEndEditing:(UITextView *)textView{
+    NSLog(@"123456 %@,%@\n", textView,textView.text);
+    
+}
+
+
+-(void)ensure{
+    self.view.backgroundColor = [UIColor colorWithRed:arc4random() % 256 / 255.0 green:arc4random() % 256 / 255.0 blue:arc4random() % 256 / 255.0 alpha:1];
+    [self cacellresignFirstResponder];
+    
+}
+-(void)cacellresignFirstResponder{
+    [_nameTextView resignFirstResponder];
+    [_describeTextView resignFirstResponder];
+    [_telephoneTextView resignFirstResponder];
+    [_mailTextView resignFirstResponder];
+    [_QQTextView resignFirstResponder];
+    [_weixinTextView resignFirstResponder];
+
+}
 
 
 
