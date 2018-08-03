522edd750f17915978d523bff6c36c0c69cae58b
huiming <youthprajna@126.com>
2018-08-03 10:21:11 +0800

    Add work order about  newVIew

--- QQA/QQA/sections/New Group5/MineTasks/WorkOrder/OneWorkOrder/OneOrderVC.m
+++ QQA/QQA/sections/New Group5/MineTasks/WorkOrder/OneWorkOrder/OneOrderVC.m
@@ -20,7 +20,7 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     self.view.backgroundColor = [UIColor yellowColor];
-    _dataOfHeaderOfTheDepartment =  [NSMutableArray array];
+    _dataOfHeaderOfTheDepartment = [NSMutableArray array];
     
     [self addHeaderView];
 }
--- QQA/QQA/sections/New Group5/MineTasks/WorkOrder/WorkOrderNameListVC.m
+++ QQA/QQA/sections/New Group5/MineTasks/WorkOrder/WorkOrderNameListVC.m
@@ -9,16 +9,75 @@
 #import "WorkOrderNameListVC.h"
 #import "OneOrderVC.h"
 
-@interface WorkOrderNameListVC ()
+#define kWORKORDERWIDTH  iphoneWidth - 20 //WORKORDERWIDTH
+#define kWORKORDERORGINh  (iphoneHeight - iphoneWidth)/2   //iphoneHeight
+#define kWORKORDERORGINHSPACE  (iphoneWidth - 20) / 20   //workOrderSpace
+
+@interface WorkOrderNameListVC ()<UITextFieldDelegate>
+@property (nonatomic, strong) UIView * addOrEditWorkOrderView;
+@property (nonatomic, strong) UILabel * workOrderTitle;
+@property (nonatomic, strong) UITextField * workOrderTextField;
 
 @end
 @implementation WorkOrderNameListVC
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    _addOrEditWorkOrderView = [UIView new];
+    _workOrderTitle = [UILabel new];
+    _workOrderTextField = [[UITextField alloc] init];
+    _workOrderTextField.delegate = self;
+    
     self.view.backgroundColor = [UIColor redColor];
     [self.navigationItem setTitle:@"工单列表"];
     self.navigationItem.rightBarButtonItem = [[UIBarButtonItem alloc] initWithBarButtonSystemItem:(UIBarButtonSystemItemAdd) target:self action:@selector(gotoOneOrderVC)];
+    [self addNewOREditWorkOrderView];
+}
+
+-(void)addNewOREditWorkOrderView{
+    _addOrEditWorkOrderView.frame = CGRectMake(10, kWORKORDERORGINh, kWORKORDERWIDTH, kWORKORDERWIDTH);
+    _addOrEditWorkOrderView.backgroundColor = [UIColor grayColor];
+    [self.view addSubview:_addOrEditWorkOrderView];
+    
+    _workOrderTitle.frame = CGRectMake(10, kWORKORDERORGINHSPACE, kWORKORDERWIDTH, kWORKORDERORGINHSPACE);
+    _workOrderTitle.textAlignment = NSTextAlignmentCenter;
+    _workOrderTitle.text = @"新建/编辑工单";
+    [_addOrEditWorkOrderView addSubview:_workOrderTitle];
+    
+    NSArray * labelNameAboutTitleANDContent = @[@"工单标题",@"工单内容"];
+    for (int i = 0; i < labelNameAboutTitleANDContent.count; i++) {
+        UILabel * nameLabel = [[UILabel alloc] initWithFrame:CGRectMake(10, (3 + i * 2)* kWORKORDERORGINHSPACE,100, kWORKORDERORGINHSPACE)];
+        nameLabel.textAlignment = NSTextAlignmentLeft;
+        nameLabel.backgroundColor = [UIColor whiteColor];
+        nameLabel.text = labelNameAboutTitleANDContent[i];
+        [_addOrEditWorkOrderView addSubview:nameLabel];
+    }
+    
+//    _workOrderTextField.frame = CGRectMake(10 + kWORKORDERWIDTH / 10 + 5 , 3 * kWORKORDERORGINHSPACE, kWORKORDERWIDTH * 9 / 10 - 5, kWORKORDERORGINHSPACE);
+    _workOrderTextField.frame = CGRectMake(115 , 3 * kWORKORDERORGINHSPACE - kWORKORDERORGINHSPACE / 2,  kWORKORDERWIDTH  - 120, kWORKORDERORGINHSPACE * 2);
+    _workOrderTextField.backgroundColor = [UIColor whiteColor];
+    _workOrderTextField.borderStyle = UITextBorderStyleLine;
+    _workOrderTextField.adjustsFontSizeToFitWidth = YES;
+    [_addOrEditWorkOrderView addSubview:_workOrderTextField];
+    
+    
+}
+
+#pragma UITextFieldDelegate
+-(BOOL)textField:(UITextField *)textField shouldChangeCharactersInRange:(NSRange)range replacementString:(NSString *)string{
+    if (textField == self.workOrderTextField) {
+        //这里的if时候为了获取删除操作,如果没有次if会造成当达到字数限制后删除键也不能使用的后果.
+        if (string.length == 0) {
+            return YES;
+        }
+        //so easy
+        else if (self.workOrderTextField.text.length >= 20) {
+            [self alert:@"工单标题不能超过20个字符!"];
+            self.workOrderTextField.text = [textField.text substringToIndex:20];
+            return NO;
+        }
+    }
+    return YES;
 }
 
 -(void)gotoOneOrderVC{
@@ -26,6 +85,20 @@
     [self.navigationController pushViewController:oneOrderVC animated:YES];
 }
 
+-(void)alert:(NSString *)str{
+    
+    NSString *title = str;
+    NSString *message = @" ";
+    NSString *okButtonTitle = @"OK";
+    UIAlertController *alertDialog = [UIAlertController alertControllerWithTitle:title message:message preferredStyle:UIAlertControllerStyleAlert];
+    UIAlertAction *okAction = [UIAlertAction actionWithTitle:okButtonTitle style:UIAlertActionStyleDefault handler:^(UIAlertAction *action) {
+        // 操作具体内容
+        // Nothing to do.
+    }];
+    [alertDialog addAction:okAction];
+    [self.navigationController presentViewController:alertDialog animated:YES completion:nil];
+    
+}
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
