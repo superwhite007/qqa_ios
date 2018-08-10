43e27df6a162794a1ec504e40f0fdc53cd234195
huiming <youthprajna@126.com>
2018-08-10 16:55:54 +0800

    Add leader view

--- QQA/QQA/sections/New Group5/MineTasks/WorkOrder/OneWorkOrder/OneOrderVC.m
+++ QQA/QQA/sections/New Group5/MineTasks/WorkOrder/OneWorkOrder/OneOrderVC.m
@@ -10,6 +10,7 @@
 #import "OneOrder.h"
 #import "OneOrderCell.h"
 #import "OneOrderCommunicationVController.h"
+#import "UIButton+WebCache.h"
 
 #define kHEADERBTNSPACE  (iphoneWidth - 320) / 5 //button间隙
 
@@ -21,6 +22,7 @@
 @property (nonatomic, assign) int pageNum;
 @property (nonatomic, assign) BOOL isDownRefresh;
 @property (nonatomic, strong) NSMutableArray *datasourceMArray;
+@property (nonatomic, strong) NSMutableArray * leadersMArray;
 
 @end
 @implementation OneOrderVC
@@ -30,6 +32,12 @@ static  NSString  * identifier = @"CELL";
         _datasourceMArray = [NSMutableArray array];
     }
     return _datasourceMArray;
+}
+-(NSMutableArray *)leadersMArray{
+    if (!_leadersMArray) {
+        _leadersMArray = [NSMutableArray new];
+    }
+    return _leadersMArray;
 }
 
 - (void)viewDidLoad {
@@ -62,6 +70,49 @@ static  NSString  * identifier = @"CELL";
 //    _workOrderNameRejectBtn.backgroundColor = [UIColor whiteColor];
 //    _agreeBTN = YES;
 //    _addOrEditWorkOrderView.frame = CGRectMake(10, kWORKORDERORGINh, kWORKORDERWIDTH, kWORKORDERWIDTH);
+}
+
+-(void)getWorkOrderListFromServerTest{
+    NSURL * url = [NSURL URLWithString:[NSString stringWithFormat:@"%@/v1/api/v2/workListDetail/index", CONST_SERVER_ADDRESS]];
+    NSMutableURLRequest * request = [NSMutableURLRequest requestWithURL:url];
+    [request setValue:@"application/json" forHTTPHeaderField:@"Content-Type"];
+    request.timeoutInterval = 10.0;
+    request.HTTPMethod = @"POST";
+    NSString *sTextPath = [NSHomeDirectory() stringByAppendingPathComponent:@"Documents/bada.txt"];
+    NSDictionary *resultDic = [NSDictionary dictionaryWithContentsOfFile:sTextPath];
+    NSString *sTextPathAccess = [NSHomeDirectory() stringByAppendingPathComponent:@"Documents/badaAccessToktn.txt"];
+    NSDictionary *resultDicAccess = [NSDictionary dictionaryWithContentsOfFile:sTextPathAccess];
+    NSMutableDictionary * mdict = [NSMutableDictionary dictionaryWithDictionary:resultDic];
+    [request setValue:resultDicAccess[@"accessToken"] forHTTPHeaderField:@"Authorization"];
+    [mdict setObject:@"IOS_APP" forKey:@"clientType"];
+    [mdict setObject:_workListIdStr forKey:@"workListId"];
+    NSError * error = nil;
+    NSData * jsonData = [NSJSONSerialization dataWithJSONObject:mdict options:NSJSONWritingPrettyPrinted error:&error];
+    request.HTTPBody = jsonData;
+    NSURLSession *session = [NSURLSession sharedSession];
+    NSURLSessionTask *task = [session dataTaskWithRequest:request
+                                        completionHandler:^(NSData *data, NSURLResponse *response, NSError *error) {
+                                            if (data != nil) {
+                                                id  dataBack = [NSJSONSerialization JSONObjectWithData:data options:NSJSONReadingMutableLeaves error:nil];
+                                                NSLog(@"dataBack:oneOrder:%@", dataBack);
+                                                if ([dataBack isKindOfClass:[NSDictionary class]]){
+                                                    if ([[dataBack objectForKey:@"message"] intValue] == 70006) {
+                                                        NSMutableArray * leaderMArray = [[[dataBack objectForKey:@"data"] objectForKey:@"data_list"] objectForKey:@"leaders"];
+                                                        NSLog(@"self.datasourceMArrayleader:%@", self.datasourceMArray);
+                                                        dispatch_async(dispatch_get_main_queue(), ^{
+                                                            
+                                                            [self getHeadOfTheDepartmentFromServder:leaderMArray];
+                                                            
+                                                        });
+                                                    }
+                                                }else if ([dataBack isKindOfClass:[NSArray class]] ) {
+                                                    [self alert:@"获取失败"];
+                                                }
+                                            }else{
+                                                [self alert:@"获取失败"];
+                                            }
+                                        }];
+    [task resume];
 }
 
 
@@ -134,7 +185,7 @@ static  NSString  * identifier = @"CELL";
     _headerView = [[UIView alloc] initWithFrame:CGRectMake(0, 0, iphoneWidth, 120)];
     _headerView.backgroundColor = [UIColor blueColor];
     [self.view addSubview:_headerView];
-    [self headerViewAddHeadOfTheDepartmentButton];
+    [self getWorkOrderListFromServerTest];
     return _headerView;
 }
 -(void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath{
@@ -142,30 +193,38 @@ static  NSString  * identifier = @"CELL";
     [self.navigationController pushViewController:oneOrderCommunicationVController animated:YES];
 }
 
--(void)headerViewAddHeadOfTheDepartmentButton{
-    [self getHeadOfTheDepartmentFromServder];
-}
--(void)getHeadOfTheDepartmentFromServder{
-    [_dataOfHeaderOfTheDepartment removeAllObjects];
-    [_dataOfHeaderOfTheDepartment addObject:@"test1"];
-    [_dataOfHeaderOfTheDepartment addObject:@"test2"];
-    [_dataOfHeaderOfTheDepartment addObject:@"test3"];
-    NSLog(@"count:::::%lu", (unsigned long)_dataOfHeaderOfTheDepartment.count);
-    //按照产品设计，最多四个候选人
+-(void)getHeadOfTheDepartmentFromServder:(NSMutableArray *)mAry{
+//    if (_dataOfHeaderOfTheDepartment.count > 0) {
+//        [_dataOfHeaderOfTheDepartment removeAllObjects];
+//    }
+    _dataOfHeaderOfTheDepartment = mAry;
     for (int i = 0; i < _dataOfHeaderOfTheDepartment.count; i++) {
         UIButton * headerOfDepartmentBtn = [UIButton buttonWithType:UIButtonTypeSystem];
         headerOfDepartmentBtn.frame = CGRectMake((i + 1) * kHEADERBTNSPACE + i * 80, 5, 80, 80);
         headerOfDepartmentBtn.backgroundColor = [UIColor redColor];
-        [_headerView addSubview:headerOfDepartmentBtn];
         headerOfDepartmentBtn.layer.cornerRadius = headerOfDepartmentBtn.frame.size.width / 2;
+        headerOfDepartmentBtn.layer.masksToBounds = YES;
+        
+        NSString * urlStr = [NSString stringWithFormat:@"%@", [_dataOfHeaderOfTheDepartment[0] objectForKey:@"img"]];
+        NSURL * url = [NSURL URLWithString:urlStr];
+        NSData *data = [NSData dataWithContentsOfURL:url];
+        UIImage * image = [UIImage imageWithData:data];
+        [headerOfDepartmentBtn setBackgroundImage:image forState:UIControlStateNormal];
+        [_headerView addSubview:headerOfDepartmentBtn];
+        
         UILabel * nameLabel = [[UILabel alloc] initWithFrame:CGRectMake((i + 1) * kHEADERBTNSPACE + i * 80, 90, 80, 25)];
         nameLabel.backgroundColor = [UIColor redColor];
+        nameLabel.textAlignment = NSTextAlignmentCenter;
+        nameLabel.text = [_dataOfHeaderOfTheDepartment[i] objectForKey:@"status"];
         [_headerView addSubview:nameLabel];
     }
     if(_dataOfHeaderOfTheDepartment.count < 4){
         UIButton * addNewHeaderOfDepartmentBtn = [UIButton buttonWithType:UIButtonTypeSystem];
         addNewHeaderOfDepartmentBtn.frame = CGRectMake((_dataOfHeaderOfTheDepartment.count + 1) * kHEADERBTNSPACE + _dataOfHeaderOfTheDepartment.count * 80, 5, 80, 80);
         addNewHeaderOfDepartmentBtn.backgroundColor = [UIColor greenColor];
+        addNewHeaderOfDepartmentBtn.layer.cornerRadius = addNewHeaderOfDepartmentBtn.frame.size.width / 2;
+        addNewHeaderOfDepartmentBtn.layer.masksToBounds = YES;
+        
         [_headerView addSubview:addNewHeaderOfDepartmentBtn];
         UILabel * nameLabel = [[UILabel alloc] initWithFrame:CGRectMake((_dataOfHeaderOfTheDepartment.count + 1) * kHEADERBTNSPACE + _dataOfHeaderOfTheDepartment.count * 80, 90, 80, 25)];
         nameLabel.backgroundColor = [UIColor redColor];
